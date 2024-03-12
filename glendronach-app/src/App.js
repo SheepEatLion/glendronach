ce59f50f535137d19f34e888b3db03fc8c089524
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 13:06:23 +0900

    feat: 홈화면 디테일화면 라우팅 적용

--- glendronach-app/src/App.js
+++ glendronach-app/src/App.js
@@ -1,25 +1,41 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/detail' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1> homepage - test </h1>
+      <img src={Process.env.PUBLIC_URL + '/home_backgournd.jpg'} />
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} />
+      </Link>
     </div>
   );
-}
+};
+
+const Detail = () => {
+  return (
+    <div>
+      <h1> detailPage - test </h1>
+      <img src={process.env.PUBLIC_URL + '/detail_12years.jpg'} />
+      <Link to="/">
+        <img src={process.env.PUBLIC_URL + '/button_back.png'} />
+      </Link>
+    </div>
+  );
+};
 
 export default App;
