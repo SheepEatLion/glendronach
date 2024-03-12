c2f364fab8b45c751034397be67ec8ed6e08702f
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 19:27:40 +0900

    fix: 함수로 변경

--- glendronach-app/src/components/HomePage.js
+++ glendronach-app/src/components/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
-const HomePage = () => {
+function HomePage() {
   return (
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
