12f833f0f2839d71047eed689f7da63829369eec
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 23:19:17 +0900

    fc

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -5,38 +5,42 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <div>
-    <img class="home" src={process.env.PUBLIC_URL + '/home_background.jpg'} alt="home" />
-      <Link to="/detail">
-        <img class="btn1" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+      <div style="position:relative;">
+      <img class="home" src={process.env.PUBLIC_URL + '/home_background.jpg'} alt="home" />
+      </div>
+        <Link to="/detail">
+          <div style="position:absolute;">
+          <img class="btn1" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn2" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn2" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn3" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn3" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn4" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn4" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn5" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn5" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn6" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn6" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn7" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn7" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
 
-      <Link to="/detail">
-        <img class="btn8" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
-      </Link>
+        <Link to="/detail">
+          <img class="btn8" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        </Link>
     </div>
   );
 };
