39d4933058f858dfb7bf4756cb6f7fef7832c008
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 22:00:53 +0900

    c

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <div class="HomePage">
-      테스트
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
