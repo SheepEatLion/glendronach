f3c201d8ac217af5d7d30c1dec56829f4c3c795f
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 21:10:41 +0900

    chore: 패키지 이동

--- glendronach-app/src/App.js
+++ glendronach-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import DetailPage from './DetailPage';
+import HomePage from './components/HomePage';
+import DetailPage from './components/DetailPage';
 
 
 function App() {
