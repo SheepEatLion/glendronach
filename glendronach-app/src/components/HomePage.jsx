0183f42b1d93af9565f97c61de2deaac2dde1d90
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 21:25:10 +0900

    fix: classname 적용

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
+    <div className='HomaPage'>
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
+    </div>
   );
 };
 
