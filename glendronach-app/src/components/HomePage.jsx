cba431e8f96a1f5de57ba97dda78f8c5de1230e4
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 22:10:35 +0900

    ㅁㅁ

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -8,6 +8,27 @@ function HomePage() {
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
+      <Link to="/detail">
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+      </Link>
     </div>
   );
 };
