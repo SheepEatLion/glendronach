9eeeaad3e909a907ff2d5b50951ecb58f84f4173
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 19:23:25 +0900

    feat: add alt

--- glendronach-app/src/components/HomePage.js
+++ glendronach-app/src/components/HomePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
       <Link to="/detail">
-        <img src={process.env.PUBLIC_URL + '/button_next.png'} />
+        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
   );
 };
