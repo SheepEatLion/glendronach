a3dd04ba5bd7abbef32f0216b1dd9e502deb07ae
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 22:23:39 +0900

    dd

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
   return (
     <div class="HomePage">
       <Link to="/detail">
-        <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+        <img class="btn1" src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
