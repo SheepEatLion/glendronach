b31c63d99af4629315a4cfb187975733b6316bd7
장준영 <junyeong.jang@wadiz.kr>
2024-03-12 21:53:32 +0900

    chore: test

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div className="HomaPage">
+    <div style={{
+      backgroundImage: 'url(../../public/home_background.jpg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'top center',
+      backgroundSize: cover
+    }}>
+      테스트
       <Link to="/detail">
         <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
       </Link>
