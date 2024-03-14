acc4ba3eb0896f1d1233c4ab2874ec5991df2902
장준영 <junyeong.jang@wadiz.kr>
2024-03-14 15:08:34 +0900

    feat: 디테일 화면

--- glendronach-app/src/components/Detail.jsx
+++ glendronach-app/src/components/Detail.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './DetailPage.css';
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 
-const DetailPage = () => {
+export default function Detail() {
+  const { years } = useParams();
+
   return (
     <div>
-      <img src={process.env.PUBLIC_URL + '/detail_12years.jpg'} />
+      <img src={process.env.PUBLIC_URL + '/detail_'+years+'.jpg'} />
       <Link to="/">
         <img src={process.env.PUBLIC_URL + '/button_back.png'} />
       </Link>
     </div>
-  );
-};
-
-export default DetailPage;
+  )
+}
