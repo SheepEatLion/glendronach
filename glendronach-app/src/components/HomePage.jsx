2e026b6e9c64fa7361f37058a97db014965a1962
장준영 <junyeong.jang@wadiz.kr>
2024-03-14 15:24:08 +0900

    feat: routing

--- glendronach-app/src/components/HomePage.jsx
+++ glendronach-app/src/components/HomePage.jsx
@@ -14,79 +14,79 @@ function HomePage() {
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/12years">
           <div class="btn1">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/15years">
           <div class="bottle15">
           <img src={process.env.PUBLIC_URL + '/bottle_15years.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/15years">
           <div class="btn2">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/18years">
           <div class="bottle18">
           <img src={process.env.PUBLIC_URL + '/bottle_18years.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/18years">
           <div class="btn3">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/21years">
           <div class="bottle21">
           <img src={process.env.PUBLIC_URL + '/bottle_21years.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/21years">
           <div class="btn4">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/28years">
           <div class="bottle28">
           <img src={process.env.PUBLIC_URL + '/bottle_28years.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/28years">
           <div class="btn5">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/29years">
           <div class="bottle29">
           <img src={process.env.PUBLIC_URL + '/bottle_29years.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/29years">
           <div class="btn6">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/portwood">
           <div class="bottleportwood">
           <img src={process.env.PUBLIC_URL + '/bottle_portwood.png'} alt="bottle" />
           </div>
         </Link>
 
-        <Link to="/detail/:years">
+        <Link to="/detail/portwood">
           <div class="btn7">
           <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
           </div>
