43587c1e84e880fca38901cc6e7168aaa1b312d4
장준영 <junyeong.jang@wadiz.kr>
2024-03-14 15:40:41 +0900

    feat: 모든 기능 구현

---
+++ glendronach-app/src/components/Detail.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import './Detail.css';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useParams } from 'react-router';
+
+
+export default function Detail() {
+  const { years } = useParams();
+  const [timer, setTimer] = useState(null);
+
+  const handleClick = () => {
+    clearTimeout(timer);
+    setTimer(setTimeout(goToHomePage, 60000));
+  };
+
+  const goToHomePage = () => {
+    window.location.href = '/';
+  };
+
+  useEffect(() => {
+    setTimer(setTimeout(goToHomePage, 60000));
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      clearTimeout(timer);
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  return (
+    <div class="mainImg">
+      <img src={process.env.PUBLIC_URL + '/detail_'+years+'.jpg'} alt="mainImg" />
+      <Link to="/">
+        <div class="back">
+        <img src={process.env.PUBLIC_URL + '/button_back.png'} alt="back" />
+        </div>
+      </Link>
+    </div>
+  )
+}
---
+++ glendronach-app/src/components/HomePage.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import './HomePage.css';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <div class="home">
+      <img src={process.env.PUBLIC_URL + '/home_background.jpg'} alt="home" />
+      </div>
+        <Link to="/detail/12years">
+          <div class="bottle12">
+          <img src={process.env.PUBLIC_URL + '/bottle_12years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/12years">
+          <div class="btn1">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/15years">
+          <div class="bottle15">
+          <img src={process.env.PUBLIC_URL + '/bottle_15years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/15years">
+          <div class="btn2">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/18years">
+          <div class="bottle18">
+          <img src={process.env.PUBLIC_URL + '/bottle_18years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/18years">
+          <div class="btn3">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/21years">
+          <div class="bottle21">
+          <img src={process.env.PUBLIC_URL + '/bottle_21years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/21years">
+          <div class="btn4">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/28years">
+          <div class="bottle28">
+          <img src={process.env.PUBLIC_URL + '/bottle_28years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/28years">
+          <div class="btn5">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/29years">
+          <div class="bottle29">
+          <img src={process.env.PUBLIC_URL + '/bottle_29years.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/29years">
+          <div class="btn6">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+
+        <Link to="/detail/portwood">
+          <div class="bottleportwood">
+          <img src={process.env.PUBLIC_URL + '/bottle_portwood.png'} alt="bottle" />
+          </div>
+        </Link>
+
+        <Link to="/detail/portwood">
+          <div class="btn7">
+          <img src={process.env.PUBLIC_URL + '/button_next.png'} alt="Next" />
+          </div>
+        </Link>
+    </div>
+  );
+};
+
+export default HomePage;
